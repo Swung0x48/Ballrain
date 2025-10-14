916bf590c2a126bfe9a421620ba4421ffc69b249
Swung0x48 <swung0x48@outlook.com>
2025-10-14 14:13:33 +0800

    [Feat]: make InputSystem accept input from outside

--- BallrainIO/src/InputSystem.cpp
+++ BallrainIO/src/InputSystem.cpp
@@ -3,8 +3,23 @@
 InputSystem::InputSystem(InputHook* input) : m_inputManager(input) {
 }
 
-void InputSystem::Process()
+void InputSystem::Process(const KeyState& state)
 {
 	auto* kbd = m_inputManager->GetKeyboardState();
-	kbd[CKKEY_UP] = KS_PRESSED;
+	for (int i = 0; i < KeyState::BR_KEY_COUNT; ++i) {
+		ApplyKeyState(kbd[m_keyMap[i]], state.states[i]);
+	}
+}
+
+void InputSystem::ApplyKeyState(unsigned char& state, char newState)
+{
+	if (newState) {
+		state = KS_PRESSED;
+	}
+	else {
+		if (state == KS_PRESSED)
+			state == KS_RELEASED;
+		else
+			state == KS_IDLE;
+	}
 }
--- BallrainIO/src/InputSystem.h
+++ BallrainIO/src/InputSystem.h
@@ -3,11 +3,34 @@
 #define _WIN32_LEAN_AND_MEAN
 #include <Windows.h>
 
+struct KeyState {
+	enum Keys {
+		BR_UP = 0,
+		BR_DOWN,
+		BR_LEFT,
+		BR_RIGHT,
+		BR_ESC,
+		BR_SHIFT,
+		BR_KEY_COUNT
+	};
+	char states[BR_KEY_COUNT];
+};
+
+
 class InputSystem {
 public:
 	InputSystem(InputHook* input);
-	void Process();
+	void Process(const KeyState& state);
+	static void ApplyKeyState(unsigned char& state, char newState);
 private:
 	HANDLE m_hPipe = INVALID_HANDLE_VALUE;
 	InputHook* m_inputManager = nullptr;
+	int m_keyMap[KeyState::BR_KEY_COUNT] = {
+		CKKEY_UP,
+		CKKEY_DOWN,
+		CKKEY_LEFT,
+		CKKEY_RIGHT,
+		CKKEY_ESCAPE,
+		CKKEY_LSHIFT
+	};
 };
